ef33f1834bc71cd7552e875bfa2c13076e3f30c1
Ben Ellis <benjamin.j.ellis@uon.edu.au>
2019-04-10 00:25:09 +0000

    morestuff

---
+++ encryption/rotation encrypt.c
@@ -0,0 +1,43 @@
+/*This section of code will perform task 1 where the string needs to be encrypted
+by any rotation by the demonstrator however it can be broken. 
+=====================================================================*/
+#include<stdio.h>
+void encrypt_me(char *,int);// declaring enrcyption function
+int main()
+{
+   char ch[50]="HELLO WORLD"; //the string to be encrypted
+  
+encrypt_me(ch,1);// calling the function
+return 0;
+}
+ 
+void encrypt_me(char *ar,int x)//function definition
+{
+   
+   for(int i=0;i<50;i++)//for loop for the entire of the string
+   {
+       int task;// declareing an integer for switch case
+       if (ar[i]>64&&ar[i]<91)
+            task=1;             //the corresponding symbol is a capital letter
+       else if(ar[i]==0)
+            break;              //the corresponding symbol is a NULL character
+       else 
+            task=0;             //any other character
+            
+        switch(task) 
+        {
+            case 0:
+                break;          //do nothing to the whitespacwe or stuff that should'nt be in the cipher
+            case 1:
+                ar[i]=ar[i]+x;              //do the encrypting
+                    if (ar[i]>90)           //if the letter is above Z in ascii 
+                    {
+                        ar[i]=ar[i]-26;     //-26 to get it back into a capital letter
+                        break;
+                    }
+                    else 
+                        break;
+       }  
+   }
+   puts(ar); 
+}
